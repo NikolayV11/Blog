01901d4af8a34daf5bfdcc044530ecaf29a4168d
Николай <wasilev-n@mail.ru>
2026-01-31 00:51:54 +0500

    Добавил объект для поста и изображений к посту чтобы добавить их модель в бд

---
+++ backend/Blog.API/Blog.DataAccess/Models/Post/Entity/Post.cs
@@ -0,0 +1,13 @@
+﻿using Blog.DataAccess.Models.Common;
+using Blog.DataAccess.Models.User.Entity;
+namespace Blog.DataAccess.Models.Post.Entity {
+    public class Post : BaseEntity {
+        public string? Content { get; set; }
+        // связь с пользователем 
+        public int UserId { get; set; }
+        public User.Entity.User Author { get; set; }
+
+        // коллекция изображений (Галерея)
+        public List<PostImages> Images { get; set; } = new();
+    }
+}
---
+++ backend/Blog.API/Blog.DataAccess/Models/Post/Entity/PostImages.cs
@@ -0,0 +1,13 @@
+﻿using Blog.DataAccess.Models.Common;
+
+namespace Blog.DataAccess.Models.Post.Entity {
+    public class PostImages: BaseEntity {
+        public string OriginalName { get; set; }
+        public string StoredName { get; set; }
+        public string ContentType { get; set; }
+
+        // Внешняя связка с постом
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
