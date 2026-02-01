8d3d711f84031826451ada8b8640978252c98498
Николай <wasilev-n@mail.ru>
2026-02-01 16:01:02 +0500

    Добавил комментарии к посту

---
+++ backend/Blog.API/Blog.Core/Models/Comment.cs
@@ -0,0 +1,34 @@
+﻿namespace Blog.Core.Models {
+    public class Comment {
+        public int Id { get; }
+
+        public string Content { get; }
+
+        public int UserId { get; }
+        public int PostId { get; }
+        public DateTime CreatedAt { get; }
+        public DateTime UpdatedAt { get; }
+        public IReadOnlyCollection<LikeCommit> Likes { get; }
+
+        private Comment(int id, string content, 
+            int userId, int postId, 
+            DateTime createdAt, DateTime updatedAt,
+            List<LikeCommit> likes) {
+            Id = id;
+            Content = content;
+            UserId = userId;
+            PostId = postId;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+            Likes = likes;
+        }
+
+        public static Comment Create(int id, string content, 
+            int userId, int postId, 
+            DateTime createdAt, DateTime updatedAt,
+            List<LikeCommit> likes) {
+
+            return new Comment(id, content, userId, postId, createdAt, updatedAt, likes);
+        }
+    }
+}
