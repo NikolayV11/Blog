a0fe5edaad6eab5bacb5b89b8e56814d2e97349a
Николай <wasilev-n@mail.ru>
2026-02-01 15:36:29 +0500

    Добавил модель ImagesPost для .Core

---
+++ backend/Blog.API/Blog.Core/Models/ImagesPost.cs
@@ -0,0 +1,17 @@
+﻿namespace Blog.Core.Models {
+    public class ImagesPost {
+        public int Id { get;  }
+        public string StoredName { get; }
+        public string Type { get;  }
+
+        private ImagesPost(int id, string stredName, string type ) {
+            Id = id;
+            StoredName = stredName;
+            Type = type;
+        }
+
+        public static ImagesPost Create(int  id, string stredName, string type ) {
+            return new ImagesPost( id, stredName, type);
+        }
+    }
+}
