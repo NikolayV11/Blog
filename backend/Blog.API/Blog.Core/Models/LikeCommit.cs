5d238277abed49f32668f349a1ac4e7eeb71d4e7
Николай <wasilev-n@mail.ru>
2026-02-01 15:48:07 +0500

    Добавил LikeCommot для Commit

---
+++ backend/Blog.API/Blog.Core/Models/LikeCommit.cs
@@ -0,0 +1,23 @@
+﻿namespace Blog.Core.Models {
+    public class LikeCommit {
+        public int Id { get; }
+        public int UserId { get; }
+        public int CommitId { get; }
+
+        // Дата поставки Like
+        public DateTime CreatedAt { get; }
+
+        private LikeCommit(
+            int id, int userId, int commitId, DateTime createdAt) {
+            Id = id;
+            UserId = userId;
+            CommitId = commitId;
+            CreatedAt = createdAt;
+        }
+
+        public  static LikeCommit Create(int id, int userId, int commitId, DateTime createdAt ) {
+            return new LikeCommit(id, userId, commitId, createdAt);
+        }
+
+    }
+}
