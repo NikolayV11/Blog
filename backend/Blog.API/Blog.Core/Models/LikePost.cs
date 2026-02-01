efbda05b2b26a4c2602e51267db91dd5fa03f6b5
Николай <wasilev-n@mail.ru>
2026-02-01 16:06:41 +0500

    Добавил LikePost для Post

---
+++ backend/Blog.API/Blog.Core/Models/LikePost.cs
@@ -0,0 +1,20 @@
+﻿namespace Blog.Core.Models {
+    public class LikePost {
+        public int Id { get; }
+        public int UserId { get; }
+        public int PostId { get; }
+
+        public DateTime CreateAt { get; }
+
+        private LikePost ( int id, int userId, int postId, DateTime createdAt ) {
+            Id = id;
+            UserId = userId;
+            PostId = postId;
+            CreateAt = createdAt;
+        }
+
+        public static LikePost  Create(int id, int userId, int postId, DateTime createdAt ) {
+            return new LikePost(id, userId, postId, createdAt);
+        }
+    }
+}
