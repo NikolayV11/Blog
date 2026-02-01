666517a3b917c5ae3206a7e87d8f52ea5b9c249b
Николай <wasilev-n@mail.ru>
2026-02-01 23:45:13 +0500

    Добавил интерфейсы для подписок и подписчиков. Добавил зависимость на .Application

---
+++ backend/Blog.API/Blog.Core/Abstractions/Service/Subscriptions/ISubscriptionService.cs
@@ -0,0 +1,6 @@
+﻿namespace Blog.Core.Abstractions.Service.Subscriptions {
+    // Команда: подписаться/отписаться
+    public interface ISubscriptionService {
+        Task<bool> ToggleFollowService ( int currentUserId, int targetUserId );
+    }
+}
