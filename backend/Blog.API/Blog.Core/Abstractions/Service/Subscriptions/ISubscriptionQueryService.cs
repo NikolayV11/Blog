8f74a00675ea20fb8f7de10a6a22c5f520866de6
Николай <wasilev-n@mail.ru>
2026-02-01 23:45:13 +0500

    Добавил интерфейсы для подписок и подписчиков. Добавил зависимость на .Contracts

---
+++ backend/Blog.API/Blog.Core/Abstractions/Service/Subscriptions/ISubscriptionQueryService.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Contracts.DTO.Follow;
+
+namespace Blog.Core.Abstractions.Service.Subscriptions {
+    // Запросы: списки людей
+    public interface ISubscriptionQueryService {
+        Task<List<UserFollowerResponse>> GetFollowingAsync ( int userId );
+        Task<List<UserFollowerResponse>> GetFollowersAsync ( int userId );
+        Task<bool> IsFollowingAsync ( int followerId, int followingId );        
+    }
+}
