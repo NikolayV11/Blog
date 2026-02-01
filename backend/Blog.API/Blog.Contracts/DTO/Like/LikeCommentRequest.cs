b037342743f1cebebca8a5a22a879f131f93f9f7
Николай <wasilev-n@mail.ru>
2026-02-01 18:25:44 +0500

    добавил рекорды для лайков комментариев

---
+++ backend/Blog.API/Blog.Contracts/DTO/Like/LikeCommentRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.Contracts.DTO.Like {
+    public record LikeCommentRequest (
+        int CommentId
+        );
+
+    public record LikeCommentResponse (
+        int CommentId,
+        int TotalLikes,
+        bool IsLikedByMe
+        );
+}
