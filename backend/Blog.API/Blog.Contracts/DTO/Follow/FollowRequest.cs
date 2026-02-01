ea46f91747a4f12f9df5b43ad055c832f55d9869
Николай <wasilev-n@mail.ru>
2026-02-01 23:24:41 +0500

    Добавил record для DTO

---
+++ backend/Blog.API/Blog.Contracts/DTO/Follow/FollowRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Blog.Contracts.DTO.Follow {
+    // С кем хотим подружится
+    public record FollowRequest (
+        int TargetUserId
+        );
+}
---
+++ backend/Blog.API/Blog.Contracts/DTO/Follow/UserFollowerResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.Contracts.DTO.Follow {
+    // кого увидем в списках (подписчики/подписки)
+    public record UserFollowerResponse (
+        int Id,
+        string FullName,
+        string? AvatarUrl
+        );
+}
