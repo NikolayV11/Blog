d56ae285f81c9e175890e0a125f341f7a8e5d1c9
Николай <wasilev-n@mail.ru>
2026-02-01 16:30:52 +0500

    Создал объект Пост и связал все в нем

---
+++ backend/Blog.API/Blog.Core/Models/Post.cs
@@ -0,0 +1,43 @@
+﻿namespace Blog.Core.Models {
+    public class Post {
+        private Post ( int id, string? content, 
+            int userId, User author, 
+            DateTime createdAt, DateTime updateAt, 
+            List<LikePost> likesPost, List<Comment> commentsPost, 
+            List<ImagePost> imagesPost ) {
+            Id = id;
+            Content = content;
+            UserId = userId;
+            Author = author;
+            CreatedAt = createdAt;
+            UpdateAt = updateAt;
+            LikesPost = likesPost;
+            CommentsPost = commentsPost;
+            ImagesPost = imagesPost;
+        }
+
+        public int Id { get; }
+        public string? Content { get; }
+        public int UserId { get; }
+        public User Author { get; }
+
+        public DateTime CreatedAt { get; }
+        public DateTime UpdateAt { get; }
+
+        // коллекции like, commit, images
+        public IReadOnlyCollection<LikePost> LikesPost { get; }
+        public IReadOnlyCollection<Comment> CommentsPost { get; }
+        public IReadOnlyCollection<ImagePost> ImagesPost { get; }
+
+        public static Post Create( int id, string? content, 
+            int userId, User author, 
+            DateTime createdAt, DateTime updateAt, 
+            List<LikePost> likesPost, List<Comment> commentsPost, 
+            List<ImagePost> imagesPost ) {
+            return new Post(id, content, 
+                userId, author, 
+                createdAt, updateAt, 
+                likesPost, commentsPost, imagesPost );
+        }
+    }
+}
