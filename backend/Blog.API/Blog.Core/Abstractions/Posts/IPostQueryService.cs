8246bea204f553810c9550bb72e94f1fe55ca350
Николай <wasilev-n@mail.ru>
2026-02-02 20:22:43 +0500

    Создал интерфейсы для работы с постами

---
+++ backend/Blog.API/Blog.Core/Abstractions/Posts/IPostQueryService.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Contracts.DTO.Post;
+
+// как будем получать посты
+namespace Blog.Core.Abstractions.Posts {
+    public interface IPostQueryService {
+        // получим все посты
+        Task<List<PostResponse>> GetPostsAsync ( );
+
+        // Получаем пост со всеми деталями
+        Task<PostResponse?> GetPostByIdAsync ( int postId );
+
+        // получаем посты определённого пользователя
+        Task<List<PostResponse>> GetUserPostsAsinc ( int userId );
+    }
+}
---
+++ backend/Blog.API/Blog.Core/Abstractions/Posts/IPostService.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Contracts.DTO.Post;
+
+// создание/удалениу поста
+namespace Blog.Core.Abstractions.Posts {
+    public interface IPostService {
+        // Возвращает ID нового поста или ошибку
+        Task<int> CreatePostAsync ( int userId, CreatePostRequest request );
+
+        // Возвращает bool после удаления
+        Task<bool> DeletePostAsync ( int userId, int postId );
+    }
+}
