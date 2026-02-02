d68b5b737e216bc2547c3d2de1b444cf6d4aa115
Николай <wasilev-n@mail.ru>
2026-02-02 15:20:30 +0500

    добавил класс для генирации и проверки хеша пароля

---
+++ backend/Blog.API/Blog.Application/Auth/PasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Core.Abstractions.Auth;
+
+namespace Blog.Application.Auth {
+    public class PasswordHasher : IPasswordHasher {
+        // Хешируем пароль перед сохранением в БД
+        public string Generate ( string password ) {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword( password );
+        }
+        // Проверяем, совпадает ли введенный пароль с хешем из базы
+        public bool VerifyPassword ( string password, string hashedPassword ) {
+            return BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
+        }
+    }
+}
