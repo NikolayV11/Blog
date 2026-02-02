450e01ba6def15b666b40b6b2adf74647b99ab4c
Николай <wasilev-n@mail.ru>
2026-02-02 17:31:29 +0500

    добавил абстрактный класс BaseCommandRepository<T> для изменения сущностей

---
+++ backend/Blog.API/Blog.DataAccess/Repositories/Base/BaseCommandRepository.cs
@@ -0,0 +1,27 @@
+﻿using Blog.Core.Abstractions.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.DataAccess.Repositories.Base {
+    // только для изменений
+    public abstract class BaseCommandRepository<TEntity> 
+        : ICreateRepository<TEntity>, IDeleteRepository<TEntity>
+        where TEntity : class {
+        private readonly BlogDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        
+        protected BaseCommandRepository(BlogDbContext context ) {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public async Task<bool> Create(TEntity entity ) {
+            await _dbSet.AddAsync( entity );
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Delete ( TEntity entity ) {
+            _dbSet.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
