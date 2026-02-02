b9f80e369e16510881514dffafb2aee003439fd7
Николай <wasilev-n@mail.ru>
2026-02-02 14:34:22 +0500

    Добавил интерфейс для входа и регистрации

---
+++ backend/Blog.API/Blog.Core/Abstractions/Service/User/IIdentityService.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Contracts.DTO.User;
+
+namespace Blog.Core.Abstractions.Service.User {
+    public interface IIdentityService {
+        Task<bool> RegisterAsync ( RegisterUserRequest request );
+
+        // Возвращает JWT токен
+        Task<string> LoginAsync( LoginUserRequest request ); 
+    }
+}
