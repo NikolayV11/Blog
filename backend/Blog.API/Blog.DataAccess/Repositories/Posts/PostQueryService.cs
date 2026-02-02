c755989281001394146c655f031633bd3c256263
Николай <wasilev-n@mail.ru>
2026-02-02 21:13:32 +0500

    получение постов и реализация DTO Post

---
+++ backend/Blog.API/Blog.DataAccess/Repositories/Posts/PostQueryService.cs
@@ -0,0 +1,63 @@
+﻿using Blog.Core.Abstractions.Service.Posts;
+using Blog.Core.Abstractions.Repository;
+using Blog.Contracts.DTO.Post;
+using Blog.DataAccess.Models.Post.Entity;
+
+namespace Blog.DataAccess.Repositories.Posts {
+    public class PostQueryService :
+        IPostQueryService
+        {
+        private readonly IGetRepository<Post> _postRepo;
+
+        public PostQueryService(IGetRepository<Post> postRepo) {
+            _postRepo = postRepo;
+        }
+
+        // получаем посты
+        public async Task<List<PostResponse>> GetPostsAsync ( ) {
+            var posts = await _postRepo.Get();
+
+            var postList = posts.Select(
+                p => new PostResponse(
+                    p.Id,
+                    p.Content,
+                    p.CreatedAt,
+                    p.UserId,
+                    $"{p.Author.FirstName} {p.Author.LastName}",
+                    p.Likes.Count,
+                    p.Commentes.Count
+                    )).ToList();
+
+            return postList;
+        }
+
+        // Получаем пост по ID
+        public async Task<PostResponse?> GetPostByIdAsync(int postId ) {
+            var posts = await _postRepo.Get();
+
+            var post = posts.FirstOrDefault(p => p.Id == postId);
+
+            if (post == null)
+                return null;
+
+            PostResponse response = new PostResponse(
+                post.Id,
+                post.Content,
+                post.CreatedAt,
+                post.UserId,
+                $"{post.Author.FirstName} {post.Author.LastName}",
+                post.Likes.Count,
+                post.Commentes.Count
+                );
+
+            return response;
+        }
+
+        public async Task<List<PostResponse>> GetUserPostsAsync(int userId ) {
+            var allPosts = await GetPostsAsync();
+            var postsUser = allPosts.Where(p => p.Id ==  userId).ToList();
+
+            return postsUser;
+        }
+    }
+}
---
+++ backend/Blog.API/Blog.DataAccess/Repositories/Posts/PostRepository.cs
@@ -0,0 +1,39 @@
+﻿using Blog.Core.Abstractions.Repository;
+using Blog.DataAccess.Models.Post.Entity;
+using Blog.DataAccess.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.DataAccess.Repositories.Posts {
+    // достаем посты и сразу добовляем автора
+    public class PostRepository : 
+        BaseQueryRepository<Post>,
+        ICreateRepository<Post>,
+        IDeleteRepository<Post>,
+        IGetRepository<Post>
+        {
+
+        public PostRepository(BlogDbContext context) : base(context) { }
+
+        // Переобределяем стандартный Get, чтобы подтянуть связи
+        public new async Task<List<Post>> Get ( ) {
+            return await _dbSet
+                .AsNoTracking()
+                .Include(p => p.Author)
+                .Include(p => p.Commentes)
+                .Include(p => p.Likes)
+                .ToListAsync();
+        }
+
+        // Создание поста
+        public async Task<bool> Create(Post entity ) {
+            await _dbSet.AddAsync(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        // Удаление поста
+        public async Task<bool> Delete(Post entity) {
+            _dbSet.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
