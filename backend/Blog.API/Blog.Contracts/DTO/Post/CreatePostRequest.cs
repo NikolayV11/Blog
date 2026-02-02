c1d0b9e2f43cac03b923677b07546f96e57d0f7e
Николай <wasilev-n@mail.ru>
2026-02-02 20:07:22 +0500

    Создал DTO поста для клиента, получение(пока без изображений), отдача(пока без изображений)

---
+++ backend/Blog.API/Blog.Contracts/DTO/Post/CreatePostRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Blog.Contracts.DTO.Post {
+    // Получение от пользователя, пока без изображений
+    public record CreatePostRequest (
+        string? Content
+        );
+}
---
+++ backend/Blog.API/Blog.Contracts/DTO/Post/PostResponse.cs
@@ -0,0 +1,12 @@
+﻿// То что отдаем клиенту
+namespace Blog.Contracts.DTO.Post {
+    public record PostResponse (
+        int Id,
+        string? Content,
+        DateTime CreatedAt,
+        int AuthorId,
+        string AuthorFullName,
+        int LikesCount,
+        int CommentsCount
+        );
+}
