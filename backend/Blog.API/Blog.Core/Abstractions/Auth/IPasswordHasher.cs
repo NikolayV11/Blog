56adca42ca212ef7215df9dd344cc0cbe5a67328
Николай <wasilev-n@mail.ru>
2026-02-02 15:00:53 +0500

    Добавил интефейс для генирации хеш для пароля и проверки пароля

---
+++ backend/Blog.API/Blog.Core/Abstractions/Auth/IPasswordHasher.cs
@@ -0,0 +1,6 @@
+﻿namespace Blog.Core.Abstractions.Auth {
+    public interface IPasswordHasher {
+        string Generate(string password);
+        bool VerifyPassword(string password, string hashedPassword);
+    }
+}
