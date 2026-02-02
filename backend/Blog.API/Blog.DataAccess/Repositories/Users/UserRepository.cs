10602705e4af4a6334fc6c51b98ce9f773e14352
Николай <wasilev-n@mail.ru>
2026-02-02 17:57:42 +0500

    Объеденил все методы в одном классе для работы с User

---
+++ backend/Blog.API/Blog.DataAccess/Repositories/Users/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿
+using Blog.Core.Abstractions.Repository;
+using Blog.Core.Abstractions.Service;
+using Blog.DataAccess.Models.User.Entity;
+using Blog.DataAccess.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.DataAccess.Repositories.Users {
+    // Наследуем Query (чтение), а остальные интерфейсы реализуем прямо здесь
+    public class UserRepository 
+        : BaseQueryRepository<User>
+        , ICreateRepository<User>
+        ,IGetByEmailRepository<User> 
+        {
+        public UserRepository(BlogDbContext context)
+            : base(context) { }
+
+        // Создание сущности
+        public async Task<bool> Create(User user ) {
+            await _dbSet.AddAsync(user);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        // поиск по Email
+        public async Task<User?> GetByEmailAsync(string email ) {
+            return await _dbSet.FirstOrDefaultAsync(u => u.Email == email);
+        }
+    }
+}
