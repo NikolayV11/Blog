40fa6bbf49865cff617c9b7fcfa0e3b7d77eb631
Николай <wasilev-n@mail.ru>
2026-02-02 17:12:02 +0500

    Абстрактный класс тлько чтение

---
+++ backend/Blog.API/Blog.DataAccess/Repositories/Base/BaseQueryRepository.cs
@@ -0,0 +1,24 @@
+﻿
+using Blog.Core.Abstractions.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.DataAccess.Repositories.Base {
+    // только чтение
+    public abstract class BaseQueryRepository<TEntity> 
+        : IGetRepository<TEntity> where TEntity : class {
+        private readonly BlogDbContext _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        protected BaseQueryRepository(BlogDbContext context ) {
+            _context = context;
+        }
+
+        public async Task<List<TEntity>> Get ( ) {
+            return await _dbSet.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TEntity?> GetById(int id ) {
+            return await _dbSet.FindAsync(id);
+        }
+    }
+}
