5b28031cb3fc718df5e3cfdd650c3ab9bf675054
Николай <wasilev-n@mail.ru>
2026-02-02 21:31:03 +0500

    добавил валидацию на пост пока нет изображений

---
+++ backend/Blog.API/Blog.Application/Validators/CreatePostValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Blog.Contracts.DTO.Post;
+
+namespace Blog.Application.Validators {
+    public class CreatePostValidator :
+        AbstractValidator<CreatePostRequest>
+        {
+        public CreatePostValidator ( ) {
+            RuleFor(p => p.Content)
+                // временная валидность на текст пока не добавим изображения
+                .NotEmpty().WithMessage("Пост не может быть пустым")
+                .MaximumLength(5000)
+                .WithMessage("Слишком длинный текст MAX: 5000.");
+        }
+
+    }
+}
