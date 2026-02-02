a1bc4b5fd2de20da8ced8137867f5c5f2c3d6042
Николай <wasilev-n@mail.ru>
2026-02-02 23:07:41 +0500

    Класс каторый просматривает классы поста, и ставил ли пользователь лайк.

---
+++ backend/Blog.API/Blog.Application/Services/Posts/PostLikeQueryService.cs
@@ -0,0 +1,31 @@
+﻿using Blog.Core.Abstractions.Service.Posts;
+using Blog.Core.Abstractions.Repository;
+using Blog.DataAccess.Models.Post.Entity;
+using Blog.Core.Abstractions.Service.LIkesPosts;
+
+namespace Blog.Application.Services.Posts {
+    internal class PostLikeQueryService
+        : ILikePostQueryService
+        {
+        private readonly IGetRepository<LikePost> _likeRepo;
+
+        public PostLikeQueryService(
+            IGetRepository<LikePost> likeRepo ) {
+            _likeRepo = likeRepo;
+        }
+
+        // общее количесто лайков у поста
+        public async Task<int> GetLikesCountAsync ( int postid ) {
+            var allLikes = await _likeRepo.Get();
+            return allLikes.Count(l => l.PostId == postid);
+        }
+
+        // Проверка ставил ли я лайк
+        public async Task<bool> IsLikedByUserAsync ( int userId, int postId ) {
+            var allLikes = await _likeRepo.Get();
+            return allLikes.Any(
+                l => l.UserId == userId 
+                && l.PostId == postId);
+        }
+    }
+}
