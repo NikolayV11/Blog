b8c069b6c593ac1b63b1d8dce4508376eb9e7b77
Николай <wasilev-n@mail.ru>
2026-02-02 23:00:09 +0500

    создал интерфейс для лайков поста

---
+++ backend/Blog.API/Blog.Core/Abstractions/Service/LIkesPosts/ILikePostQueryService.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.Core.Abstractions.Service.LIkesPosts {
+    public interface ILikePostQueryService {
+        // узнать общее количество лайков поста
+        Task<int> GetLikesCountAsync ( int postid );
+
+        // проверка ставил ли пользователь лайк
+        Task<bool> IsLikedByUserAsync ( int userId, int postId );
+    }
+}
