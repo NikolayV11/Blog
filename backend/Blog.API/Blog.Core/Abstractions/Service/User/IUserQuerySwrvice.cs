9568338d488b56d87371e0a45cd9dce98964a43c
Николай <wasilev-n@mail.ru>
2026-02-02 14:41:58 +0500

    Добавил интерфейс для поиска пользователя по id, email, и для определения уникального Email

---
+++ backend/Blog.API/Blog.Core/Abstractions/Service/User/IUserQuerySwrvice.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Core.Models;
+
+namespace Blog.Core.Abstractions.Service.User {
+    // Запрос для поиска профила
+    public interface IUserQuerySwrvice {
+        Task<Models.User?> GetByIdAsync ( int id );
+        Task<Models.User?> GetByEmailAsync( string email );
+        Task<bool> IsEmailUniqueAsync ( string email );
+    }
+}
