089e620ae91dafda23296a722c40d79ab0a13659
Николай <wasilev-n@mail.ru>
2026-02-02 14:27:48 +0500

    Добавил сущности для входа и регистрации

---
+++ backend/Blog.API/Blog.Contracts/DTO/User/LoginUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.Contracts.DTO.User {
+    // Вход
+    public record LoginUserRequest (
+        string Email,
+        string Password
+        );
+    
+}
---
+++ backend/Blog.API/Blog.Contracts/DTO/User/RegisterUserRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog.Contracts.DTO.User {
+    // Регистрация
+    public record RegisterUserRequest (
+        string Email,
+        string Password,
+        string FirstName,
+        string LastName,
+        string? Surname,
+        string Phone,
+        DateTime Birthday
+        );
+}
