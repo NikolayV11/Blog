0c3757a75be45564983eedbe7b354332c18c37f4
Николай <wasilev-n@mail.ru>
2026-02-02 18:45:26 +0500

    Реализовать UserQueryService и сопоставление доменов.

---
+++ backend/Blog.API/Blog.Application/Mapping/UserMapping.cs
@@ -0,0 +1,22 @@
+﻿using User = Blog.Core.Models.User;
+using UserEntity = Blog.DataAccess.Models.User.Entity.User;
+
+namespace Blog.Application.Mapping {
+    public static class UserMapping {
+        public static User ToDomain (this UserEntity entity ) {
+            var(user, error) = User.Create(
+                entity.Id,
+                entity.LastName,
+                entity.FirstName,
+                entity.Surname,
+                entity.Birthday,
+                entity.Email,
+                entity.Phone,
+                entity.AvatarId ?? 0,
+                null
+                ); // Здесь можно добавить маппинг Image, если он подгружен
+
+            return user!;
+        }
+    }
+}
---
+++ backend/Blog.API/Blog.Application/Services/User/UserQueryService.cs
@@ -0,0 +1,40 @@
+﻿using Blog.Core.Abstractions.Service.User;
+using Blog.Core.Abstractions.Repository;
+// чтобы заработал .ToDomain()
+using Blog.Application.Mapping;
+
+using UserEntity = Blog.DataAccess.Models.User.Entity.User;
+using UserDomain = Blog.Core.Models.User;
+using Blog.Core.Abstractions.Service;
+
+namespace Blog.Application.Services.User {
+    public class UserQueryService : IUserQueryService {
+        private readonly IGetByIdRepository<UserEntity> _idRepo;
+        private readonly IGetByEmailRepository<UserEntity> _emailRepo;
+
+        public UserQueryService ( 
+            IGetByIdRepository<UserEntity> idRepo, 
+            IGetByEmailRepository<UserEntity> emailRepo ) {
+            _idRepo = idRepo;
+            _emailRepo = emailRepo;
+        }
+
+        // Проверка уникальности для IdentityService
+        public async Task<bool> IsEmailUniqueAsync ( string email ) {
+            var user = await _emailRepo.GetByEmailAsync(email);
+            return user == null;
+        }
+
+        // Получение по ID (с маппингом в доменную модель)
+        public async Task<UserDomain?> GetByIdAsync (int id ) {
+            var entity = await _idRepo.GetById(id);
+            return entity?.ToDomain();
+        }
+
+        // Получение по Email
+        public async Task<UserDomain?> GetByEmailAsync(string email) {
+            var entity = await _emailRepo.GetByEmailAsync (email);
+            return entity?.ToDomain();
+        }
+    }
+}
