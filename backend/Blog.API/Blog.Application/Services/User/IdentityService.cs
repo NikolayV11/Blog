ad40d781f27173bcb6bb3a7cca93f0c891349e57
Николай <wasilev-n@mail.ru>
2026-02-05 12:04:07 +0500

    LoginAsync реализовали метод входа

--- backend/Blog.API/Blog.Application/Services/User/IdentityService.cs
+++ backend/Blog.API/Blog.Application/Services/User/IdentityService.cs
@@ -1,6 +1,7 @@
 ﻿using Blog.Contracts.DTO.User;
 using Blog.Core.Abstractions.Auth;
 using Blog.Core.Abstractions.Repository;
+using Blog.Core.Abstractions.Service;
 using Blog.Core.Abstractions.Service.User;
 using UserEntity = Blog.DataAccess.Models.User.Entity.User;
 
@@ -9,18 +10,21 @@ namespace Blog.Application.Services.User {
         private readonly IUserQueryService _userQuery;
         private readonly IPasswordHasher _hasher;
         private readonly ICreateRepository<UserEntity> _createRepo;
+        private readonly IGetByEmailRepository<UserEntity> _emailRepo;
 
-        public IdentityService ( 
-            IUserQueryService userQuery, 
-            IPasswordHasher hasher, 
-            ICreateRepository<UserEntity> createRepo ) {
+        public IdentityService (
+            IUserQueryService userQuery,
+            IPasswordHasher hasher,
+            ICreateRepository<UserEntity> createRepo,
+            IGetByEmailRepository<UserEntity> emailRepo ) {
             _userQuery = userQuery;
             _hasher = hasher;
             _createRepo = createRepo;
+            _emailRepo = emailRepo;
         }
         public async Task<bool> RegisterAsync ( RegisterUserRequest request ) {
             // 1. проверяем уникальность
-            if(!await _userQuery.IsEmailUniqueAsync(request.Email)) {
+            if (!await _userQuery.IsEmailUniqueAsync(request.Email)) {
                 throw new Exception("Email уже занят.");
             }
 
@@ -39,13 +43,33 @@ namespace Blog.Application.Services.User {
             };
 
             // 4. Сохраняем в бд
-            return await _createRepo.Create( newUser );
+            return await _createRepo.Create(newUser);
         }
 
         // Сделаем, когда подключим JWT
         public async Task<string> LoginAsync ( LoginUserRequest request ) {
-            throw new NotImplementedException();
-        }
+            // 1. Ищем пользователя в базе через QueryService
+            // Нам нужен доменный пользователь, но чтобы проверить пароль,
+            // нам нужен доступ к Entity (где лежит хеш)
+            // Поэтому воспользуемся репозиторием напрямую или расширим QueryService
+            var userEntity = await _emailRepo.GetByEmailAsync(request.Email); // убедимся что он возвращает сущность с хешом
+            if (userEntity == null) {
+                throw new Exception("Пользователь с таким Email не найден.");
+            }
 
+            // 2. Проверяем пароль
+            // requst.Password - "чистый" пароль от пользователя
+            // userEntity.PasswordHash из бд
+            var isPasswordValid = _hasher.VerifyPassword(request.Password, userEntity.PasswordHash);
+
+            if (( !isPasswordValid ) {
+                throw new Exception("Неверный пароль");
+            }
+
+            return "Успешный вход";
+
+        }
     }
+
+}
 }
